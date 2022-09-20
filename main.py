b0ff3641e8c818613153553c49b2ba64d1a37b50
mygithub <captainsurfervideos@gmail.com>
2022-09-19 18:45:01 -0700

    main

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print("Hello World")
+{
+ "cells": [
+  {
+   "cell_type": "markdown",
+   "metadata": {},
+   "source": [
+    "# List and Dictionary\n",
+    "\n",
+    "---\n",
+    "- title: List and Dictionary\n",
+    "- toc: true\n",
+    "- comments: true\n",
+    "- categories: [Notebook]\n",
+    "---"
+   ]
+  }
+ ],
+ "metadata": {
+  "language_info": {
+   "name": "python"
+  },
+  "orig_nbformat": 4
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
